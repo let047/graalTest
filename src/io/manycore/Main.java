1a62247a37c96327b07056a88972d8a4f9036dd3
nicotop <ntoper@gmail.com>
2020-10-19 15:19:27 +0200

    Initial commit

---
+++ src/io/manycore/Main.java
@@ -0,0 +1,18 @@
+package io.manycore;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        boolean flag = false;
+        if (System.getenv("PROD").equals("1"));
+            flag = true;
+        if (flag) {
+            for (int methodToCallCount=0; methodToCallCount <= 7; methodToCallCount++) {
+                for (int classCount = 0; classCount <= 15; classCount++) {
+                    Class thisClass = Class.forName("io.manycore.reflection.Meng" + classCount);
+                    System.out.println(thisClass.toString());
+                }
+            }
+        }
+    }
+}
